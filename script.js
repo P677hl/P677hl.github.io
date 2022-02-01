97c94247f8f90824bbad292eaf796731bf9db7fc
I'm Prayoga <72582850+P677hl@users.noreply.github.com>
2022-02-01 10:33:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var site = 'https://imprayoga.rf.gd/';
+var siteHtml = '<iframe id="ifr" src="' + site + '" style="border:0;width:100%;height:100vh;padding:0;"></iframe>';
+var isNew = document.getElementById("website");
+isNew.innerHTML = siteHtml;
